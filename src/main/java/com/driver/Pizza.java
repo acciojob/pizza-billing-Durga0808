5a7d9f6bd8e6cb4b9ecf0e5cf6d05f620d4c0d3c
Durga0808 <117895509+Durga0808@users.noreply.github.com>
2023-03-19 18:21:08 +0530

    Update Pizza.java

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,13 +2,37 @@ package com.driver;
 
 public class Pizza {
 
+    /*the price here will be printed on bill*/
     private int price;
+    //all variable prices
+    int baseprice;
+    int ExtraCheeseprice;
+    int ExtraToppingsprice;
+    int papaerbagprice;
+
     private Boolean isVeg;
+
     private String bill;
+    private boolean ischeeseadded;
+    private boolean istoppingdadded;
+    private boolean istakeawayadded;
+    private boolean isgetbill;
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.ExtraCheeseprice=80;
+        this.papaerbagprice=20;
+        if(this.isVeg==true){
+            this.baseprice=300;
+            this.ExtraToppingsprice=70;
+        }else{
+            this.baseprice=400;
+            this.ExtraToppingsprice=120;
+        }
+        this.price=baseprice;
     }
 
     public int getPrice(){
@@ -17,18 +41,44 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(ischeeseadded==false){
+            this.price=this.price+ExtraCheeseprice;
+            ischeeseadded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(istoppingdadded==false){
+            this.price=this.price+ExtraToppingsprice;
+            istoppingdadded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(istakeawayadded==false){
+            this.price=this.price+papaerbagprice;
+            istakeawayadded=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(isgetbill==false){
+            this.bill="Base Price Of The Pizza: "+this.baseprice+"\n";
+            if(ischeeseadded==true){
+                this.bill=this.bill+"Extra Cheese Added: "+this.ExtraCheeseprice+"\n";
+            }
+            if(istoppingdadded==true){
+                this.bill=this.bill+"Extra Toppings Added: "+this.ExtraToppingsprice+"\n";
+            }
+            if(istakeawayadded==true){
+                this.bill=this.bill+"Paperbag Added: "+this.papaerbagprice+"\n";
+            }
+            this.bill=this.bill+"Total Price: "+this.price;
+            isgetbill=true;
+        }
         return this.bill;
     }
 }
